8d546913eeab64b92193e25bb24c665f06461f00
Jarva <4622609+Jarva@users.noreply.github.com>
2025-09-19 11:03:00 +0100

    feat: add Deno support

--- src/useScreenSize.ts
+++ src/useScreenSize.ts
@@ -1,6 +1,8 @@
 import { useStdout } from "ink";
 import { useCallback, useEffect, useState } from "react";
 
+const isDeno = typeof Deno !== 'undefined';
+
 /**
  * Returns an up to date screen size object with `height` and `width` properties
  * that reflect the current size of the terminal (rows and columns in stdout).
@@ -18,8 +20,14 @@ export function useScreenSize() {
       setSize(getSize());
     }
     stdout.on("resize", onResize);
+    if (isDeno) {
+      Deno.addSignalListener('SIGWINCH', onResize);
+    }
     return () => {
       stdout.off("resize", onResize);
+      if (isDeno) {
+        Deno.removeSignalListener('SIGWINCH', onResize);
+      }
     };
   }, [stdout, getSize]);
 
